fbff59d32a950bf40d1744b9b83cb5578d3c476c
TuomasLattila <tuomas.lattila@outlook.com>
2023-07-09 18:28:00 +0300

    Added chart made with frappe charts. The chart shows the result of the parliament elections in 2023 in the whole country

--- js/script.js
+++ js/script.js
@@ -1,7 +1,69 @@
-
 //URLs for fetching data --------------------------------------
 const geoURL = 'https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326'
-// ------------------------------------------------------------
+const chartURL = 'https://statfin.stat.fi:443/PxWeb/api/v1/fi/StatFin/evaa/statfin_evaa_pxt_13sw.px'
+//-------------------------------------------------------------
+//Body JSON Query ---------------------------------------------
+const jsonQuery = {
+    "query": [
+      {
+        "code": "Vuosi",
+        "selection": {
+          "filter": "item",
+          "values": [
+            "2023"
+          ]
+        }
+      },
+      {
+        "code": "Sukupuoli",
+        "selection": {
+          "filter": "item",
+          "values": [
+            "SSS"
+          ]
+        }
+      },
+      {
+        "code": "Puolue",
+        "selection": {
+          "filter": "item",
+          "values": [
+            "03",
+            "02",
+            "01",
+            "04",
+            "05",
+            "06",
+            "07",
+            "08",
+            "09"
+          ]
+        }
+      },
+      {
+        "code": "Vaalipiiri ja kunta vaalivuonna",
+        "selection": {
+          "filter": "item",
+          "values": [
+            "SSS"
+          ]
+        }
+      },
+      {
+        "code": "Tiedot",
+        "selection": {
+          "filter": "item",
+          "values": [
+            "evaa_aanet"
+          ]
+        }
+      }
+    ],
+    "response": {
+      "format": "json-stat2"
+    }
+}
+//-------------------------------------------------------------
 
 //Initializing the map with borders of different municipalities in Finland.
 const initMap = async () => {
@@ -22,19 +84,58 @@ const initMap = async () => {
     map.fitBounds(geoJson.getBounds())
 }
 
+//Method for creating features for the map
 const getFeature = (feature, layer) => {
     const id = feature.id
     if (!id) return;
     layer.bindTooltip(feature.properties.nimi)
 }
+//method for building the chart.
+const buildChart = async (body) => {
+    const data = await fetchChartData(chartURL, body)
+    console.log(data)
 
-//Fetching method.
+    const year = Object.values(data.dimension.Vuosi.category.label)[0]
+    const area = Object.values(data.dimension["Vaalipiiri ja kunta vaalivuonna"].category.label)[0]
+    const parties = Object.values(data.dimension.Puolue.category.label)
+    const values = data.value
+
+    const dataArray = [{name: area, values: values}]
+    console.log(values)
+
+    const chartData = {
+        labels: parties,
+        datasets: dataArray
+    }
+
+    const chart = new frappe.Chart("#chart", {
+        title: area + " (" + year + "):",
+        data: chartData,
+        type: 'bar',
+    })
+}
+
+//Fetching methods:
 const fetchData = async (url) => {
     const dataPromise = await fetch(url)
     const dataJSON = await dataPromise.json()
-    console.log(dataJSON)
+    //console.log(dataJSON)
     return dataJSON
 }
 
-//Initialitzation call
+const fetchChartData = async (url, body) => {
+    const dataPromise = await fetch(url, {
+        method: "POST",
+        headers: {"content-type": "application/json"},
+        body: JSON.stringify(body)
+    })
+    if (!dataPromise.ok)    {
+        return;
+    }
+    const dataJSON = await dataPromise.json()
+    return dataJSON
+}
+
+//Initialitzation calls
 initMap()
+buildChart(jsonQuery)
